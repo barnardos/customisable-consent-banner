68b049cfeb8a64c3ad046b1046dfe01e7b24368e
Clare Dowson <clare.dowson@barnardos.org.uk>
2022-01-27 13:00:21 +0000

    cookie privacy added

--- consent-banner.node.js
+++ consent-banner.node.js
@@ -5,6 +5,21 @@ module.exports = () => {
     return;
   }
 
+  let privacyURL; 
+  let cookieURL;
+
+  if(process.env.PRIVACY_URL) {
+    privacyURL = options.privacyURL;
+  } else {
+    privacyURL = 'https://www.barnardos.org.uk/privacy-notice';
+  } 
+
+  if(process.env.COOKIE_URL) {
+    cookieURL = options.cookieURL;
+  } else {
+    cookieURL = 'https://www.barnardos.org.uk/cookie-notice';
+  }
+
   const getCookieValue = (name) => {
     const result = document.cookie.match(
       `(^|[^;]+)\\s*${name}\\s*=\\s*([^;]+)`,
@@ -49,7 +64,7 @@ module.exports = () => {
     heading.id = "dialog-title";
     const text = document.createElement('p');
     text.id = "dialog-description";
-    text.innerHTML = 'We use cookies to improve your experience on our site, show you personalised marketing and information and to help us understand how you use the site. By pressing accept, you agree to us storing those cookies on your device. By pressing reject, you refuse the use of all cookies except those that are essential to the running of our website. See our <a href="https://www.barnardos.org.uk/privacy-notice">privacy policy</a> and <a href="https://www.barnardos.org.uk/cookie-notice">cookie notice</a> for more details.';
+    text.innerHTML = `We use cookies to improve your experience on our site, show you personalised marketing and information and to help us understand how you use the site. By pressing accept, you agree to us storing those cookies on your device. By pressing reject, you refuse the use of all cookies except those that are essential to the running of our website. See our <a href="${privacyURL}">privacy policy</a> and <a href="${cookieURL}">cookie notice</a> for more details.`;
     const style = document.createElement('style');
     style.textContent = "._barnardos-cookie-overlay{z-index:3;position:fixed;top:0;left:0;width:100%;height:100%;background-color: rgba(0,0,0,0.7);}._barnardos-consent-banner {background-color:#fff;padding:0.5rem 1rem 1rem;position:fixed;top:50%;left:50%;width:90%;max-width:36rem;transform:translate(-50%,-50%);z-index:4}._barnardos-consent-banner:focus{outline:none}.cooke-policy h2 {margin-right:2rem}._barnardos-consent-banner p {display:inline-block;margin:0.5rem 0 1.5rem;vertical-align:middle}._barnardos-consent-banner div{display:inline-block;white-space:nowrap}._barnardos-consent-banner button {appearance: none; background-color: #558200; border: 1px solid #558200; border-radius: 0; color: #fff; display: inline-block; font-size: 1.125rem; font-weight: 800; letter-spacing: 0; line-height: 1.5rem; padding: 0.5rem 2rem; text-align: center; user-select: none; vertical-align: middle; white-space: nowrap; margin:0 1em 0 0;}._barnardos-consent-banner button:hover, ._barnardos-consent-banner button:focus { background-color: #192700; border-color: #192700; }._barnardos-consent-banner a {text-decoration:underline}._barnardos-consent-banner ._barnardos-cookie-close{position:absolute;right:0;top:0;margin:0;line-height:1;padding:0.5rem}";
     consentBanner.appendChild(style);
